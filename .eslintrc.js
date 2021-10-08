19ff6f56ad916f3277498d450986eedaaa1d5ad2
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-10-08 21:32:26 +0800

    稍微放宽规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: 'airbnb-typescript-prettier',
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
 }
