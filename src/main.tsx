3d96213a5ac6cdd99370445810b9fcb4be5b7bed
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-10-08 21:21:59 +0800

    换yarn，加eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
-import 'antd/dist/antd.css';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { initLocalStorage } from './providers/configProvider';
-import { initDataStore } from './providers/dataProvider';
-import { createProvider } from './providers/socketIoProvider';
-import Router from './routers/routers';
-import './styles/global.scss';
+import 'antd/dist/antd.css'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { initLocalStorage } from './providers/configProvider'
+import { initDataStore } from './providers/dataProvider'
+import { createProvider } from './providers/socketIoProvider'
+import Router from './routers/routers'
+import './styles/global.scss'
 
-moment('zh-cn');
+moment('zh-cn')
 
 // 初始化数据
-initLocalStorage();
-initDataStore();
+initLocalStorage()
+initDataStore()
 
 ReactDOM.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
   document.getElementById('root'),
-);
+)
