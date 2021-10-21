34c1c737709f0904de57a2c980e29f7b860b5d49
RainbowBird <rbxin2003@outlook.com>
2021-10-21 23:48:20 +0800

    feat: 修改sidebar菜单样式

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,5 +1,5 @@
 import { MenuOutlined, SearchOutlined } from '@ant-design/icons'
-import { Button, Input } from 'antd'
+import { Input } from 'antd'
 import React, { useState } from 'react'
 import styles from './AppSidebar.module.scss'
 import { SidebarRooms } from './SidebarRooms'
@@ -12,9 +12,9 @@ export default function AppSidebar() {
   return (
     <div className={styles.chatSidebar}>
       <div className={styles.sidebarHead}>
-        <Button shape="round" className={styles.menuButton}>
+        <button className={styles.menuButton}>
           <MenuOutlined className={styles.menuIcon} />
-        </Button>
+        </button>
         {
           // <Popover title={bot.uin} content={bot.nickname}>
           // <Avatar size="large" src={getUserAvatarUrl(bot.uin)} />
@@ -28,11 +28,17 @@ export default function AppSidebar() {
       <div className={styles.sidebarContent}>
         <div className={styles.foldersTabs}>
           {/* TODO: 支持自定义文件夹 */}
-          <Button.Group>
-            <Button onClick={() => setFolder(FolderType.All)}>全部会话</Button>
-            <Button onClick={() => setFolder(FolderType.Friends)}>好友</Button>
-            <Button onClick={() => setFolder(FolderType.Group)}>群组</Button>
-          </Button.Group>
+          <div>
+            <button className={folder === FolderType.All ? styles.folderActive : ''}
+                    onClick={() => setFolder(FolderType.All)}>全部会话
+            </button>
+            <button className={folder === FolderType.Friends ? styles.folderActive : ''}
+                    onClick={() => setFolder(FolderType.Friends)}>好友
+            </button>
+            <button className={folder === FolderType.Group ? styles.folderActive : ''}
+                    onClick={() => setFolder(FolderType.Group)}>群组
+            </button>
+          </div>
         </div>
         <div className={styles.tabsContainer}>
           <SidebarRooms folder={folder} />
