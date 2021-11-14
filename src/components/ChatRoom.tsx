e6b6da595aa1efdf44c7d42d15c10a8a0746afdb
RainbowBird <rbxin2003@outlook.com>
2021-11-14 15:56:19 +0800

    feat: 新增随机用户名颜色，修改好多好多的样式w

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -4,13 +4,14 @@ import { useDispatch } from 'react-redux'
 import { putMessages } from '../actions/room'
 import { getMessages } from '../adapters/room'
 import { Message, Room } from '../types/RoomTypes'
-import styles from './AppContainer.module.scss'
+import styles from './ChatRoom.module.scss'
 import ChatBubble from './ChatRoom/ChatBubble'
 
 export default function ChatRoom({ room }: { room: Room }) {
   const dispatch = useDispatch()
   const [messages, setMessages] = useState<Message[] | undefined>()
   
+  // TODO: 监听消息变动
   const attachEvents = () => {
   
   }
@@ -28,10 +29,20 @@ export default function ChatRoom({ room }: { room: Room }) {
   }, [room.roomId])
   
   return (
-    <div className={styles.chatContainer}>
-      {messages?.map((i: Message) => {
-        return <ChatBubble key={i._id} message={i} />
-      })}
+    <div>
+      <div className={styles.chatTopBar}>
+        <span className={styles.chatTopBarAvatar}>
+          <img src={room.avatar} alt="avatar" />
+        </span>
+        <span className={styles.chatTopBarTitle}>
+          {room.roomName}
+        </span>
+      </div>
+      <div className={styles.chatBubbles}>
+        {messages?.map((i: Message) => {
+          return <ChatBubble key={i._id} message={i} />
+        })}
+      </div>
     </div>
   )
 }
--- src/components/ChatRoom/ChatBubble/index.tsx
+++ src/components/ChatRoom/ChatBubble/index.tsx
@@ -1,16 +1,19 @@
 import { Avatar } from 'antd'
 import { Message } from '../../../types/RoomTypes'
 import { getUserAvatarUrl } from '../../../utils/apis'
+import { randomUsernameColor } from '../../../utils/tools'
 import styles from './bubble.module.scss'
 
 export default function ChatBubble({ message }: { message: Message }) {
+  // TODO: 进入房间每个人获取一个颜色
+  
   return (
     <div className={styles.bubble}>
       <Avatar src={getUserAvatarUrl(message.senderId as number)} />
       <div className={styles.message}>
-        <span>{message.username}</span>
-        <p>{message.content}</p>
-        <span>{message.timestamp}</span>
+        <span className={styles.username} style={{ color: randomUsernameColor() }}>{message.username}</span>
+        <p className={styles.content}>{message.content}</p>
+        <span className={styles.timestamp}>{message.timestamp}</span>
       </div>
     </div>
   )
