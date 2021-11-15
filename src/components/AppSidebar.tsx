92dd3a9526d71a7a37643b03bb904ff881c8a63b
凌莞 <i@gao4.pw>
2021-11-15 19:43:59 +0800

    侧边栏 header 优化

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -12,37 +12,38 @@ export type FolderType = 'All' | 'Friends' | 'Group'
 export default function AppSidebar() {
   const [folder, setFolder] = useState<FolderType>('All')
   const state: RootState = useStore().getState()
-  
+
   return (
     <div className={styles.chatSidebar}>
       <div className={styles.sidebarHead}>
-        <button className={styles.menuButton}>
-          <Avatar src={getUserAvatarUrl(state.onlineData?.uin as number)} />
-          {
-            // MenuOutlined className={styles.menuIcon}
-          }
-        </button>
-        <div className={styles.searchBox}>
-          <Input placeholder="Search" className={styles.searchInput} />
-          <SearchOutlined className={styles.searchIcon} />
+        <div className={styles.sidebarTitle}>
+          <button className={styles.menuButton}>
+            <Avatar src={getUserAvatarUrl(state.onlineData?.uin as number)} />
+            {
+              // MenuOutlined className={styles.menuIcon}
+            }
+          </button>
+          <div className={styles.searchBox}>
+            <Input placeholder="Search" className={styles.searchInput} />
+            <SearchOutlined className={styles.searchIcon} />
+          </div>
         </div>
-      </div>
-      <div className={styles.sidebarContent}>
         <div className={styles.foldersTabs}>
           {/* TODO: 支持自定义文件夹 */}
           <div>
-            <button className={folder === 'All' ? styles.folderActive : ''}
-                    onClick={() => setFolder('All')}>全部会话
+            <button className={folder === 'All' ? styles.folderActive : ''} onClick={() => setFolder('All')}>
+              全部会话
             </button>
-            <button className={folder === 'Friends' ? styles.folderActive : ''}
-                    onClick={() => setFolder('Friends')}>好友
+            <button className={folder === 'Friends' ? styles.folderActive : ''} onClick={() => setFolder('Friends')}>
+              好友
             </button>
-            <button className={folder === 'Group' ? styles.folderActive : ''}
-                    onClick={() => setFolder('Group')}>群组
+            <button className={folder === 'Group' ? styles.folderActive : ''} onClick={() => setFolder('Group')}>
+              群组
             </button>
           </div>
         </div>
-        
+      </div>
+      <div className={styles.sidebarContent}>
         <SidebarRooms folder={folder} />
       </div>
     </div>
