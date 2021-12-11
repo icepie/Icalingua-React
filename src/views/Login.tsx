af7a7a68d0eaa03968f0c53904191afa3eaa4f80
RainbowBird <rbxin2003@outlook.com>
2021-12-12 00:48:17 +0800

    perf: 实现登录页面样式

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,3 +1,4 @@
+import { Button, Grid, TextField } from '@mui/material'
 import { getConfig } from 'providers/configProvider'
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -9,18 +10,28 @@ export default function Login() {
     if (!requireLogin) navigate('/')
   }, [])
 
-  // const [form] = Form.useForm()
+  // const [form] = FormGroup.useForm()
 
-  // useEffect(() => {
-  //   form.setFieldsValue(getConfig())
-  // }, [])
-  //
-  // const login = async () => {
-  //   saveConfig(form.getFieldsValue())
-  //   navigate('/', { replace: true }) // 不在 history 里留记录，因为不应该留
-  // }
+  useEffect(() => {
+    // form.setFieldsValue(getConfig())
+  }, [])
 
-  return <div />
+  const login = async () => {
+    // saveConfig(form.getFieldsValue())
+    navigate('/', { replace: true }) // 不在 history 里留记录，因为不应该留
+  }
+
+  return (
+    <form onSubmit={login} style={{ width: '60%', margin: '6rem auto' }}>
+      <Grid container justifyContent="center" flexDirection="column" gap={2}>
+        <h1 style={{ textAlign: 'center' }}>连接服务器</h1>
+        <TextField label="服务器地址" name="server" />
+        <TextField label="私钥" name="privateKey" />
+
+        <Button onClick={login}>登录</Button>
+      </Grid>
+    </form>
+  )
   // return (
   //  <div>
   //    <Row className="box" justify="center">
